0ae18209ced7588a9056210fe1542b7577dad8f2
John Winstead <brooswi@gmail.com>
2025-08-09 20:21:26 -0700

    chore: remove VoxelDebugger utility script

--- Assets/Scripts/MagicAVoxel/Scripts/Vox/VoxAsset.cs
+++ Assets/Scripts/MagicAVoxel/Scripts/Vox/VoxAsset.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using System;
 
 [System.Serializable]
 public class VoxAsset : ScriptableObject
 {
     public byte[] rawData;
+
+    // Fired whenever a .vox asset is (re)imported
+    public static event Action<VoxAsset> OnAnyReimported;
+
+    internal static void RaiseReimported(VoxAsset asset)
+    {
+        OnAnyReimported?.Invoke(asset);
+    }
 }
